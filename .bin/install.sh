ebefa5aadb74b0b04d03b73f95d3c8a1e6b5aab6
Deepak Bhalla <dpkrjb@gmail.com>
2017-07-01 19:34:06 +0100

    Removed bootstrapping from install

--- .bin/install.sh
+++ .bin/install.sh
@@ -45,13 +45,3 @@ apps=(
 )
 
 brew install "${apps[@]}"
-
-# --------------------------------------------
-# Node (for Mac)
-# --------------------------------------------
-rm -rf /usr/local/lib/node_modules
-brew uninstall node
-brew install node --without-npm
-echo prefix=~/.npm-packages >> ~/.npmrc
-curl -L https://www.npmjs.com/install.sh | sh
-
