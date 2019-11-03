56cf11c340b969de556acfe370d168ecce8e5c44
Deepak Bhalla <dpkrjb@gmail.com>
2019-11-03 19:45:49 +0000

    add go dbg support

--- .shell/_go.sh
+++ .shell/_go.sh
@@ -4,4 +4,8 @@ export GOPATH=$HOME/.go
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
 
-alias godbg="dlv debug --headless --listen=:2345 --api-version=2"
+godbg() {
+  gofile="$1"
+  shift
+  dlv debug --headless --listen=:2345 --api-version=2 "$gofile" -- "$@"
+}
